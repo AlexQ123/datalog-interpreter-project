ebab272e7a10f203f6100916288f4008a113a013
Alex Qian <qianalex8@gmail.com>
2021-09-08 16:07:33 -0600

    Added starter code

---
+++ cmake-build-debug/CMakeFiles/project_1.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/alexq/CS236/project-1/Project1_Starter_Code/ColonAutomaton.cpp" "C:/Users/alexq/CS236/project-1/cmake-build-debug/CMakeFiles/project_1.dir/Project1_Starter_Code/ColonAutomaton.cpp.obj"
+  "C:/Users/alexq/CS236/project-1/Project1_Starter_Code/ColonDashAutomaton.cpp" "C:/Users/alexq/CS236/project-1/cmake-build-debug/CMakeFiles/project_1.dir/Project1_Starter_Code/ColonDashAutomaton.cpp.obj"
+  "C:/Users/alexq/CS236/project-1/Project1_Starter_Code/Lexer.cpp" "C:/Users/alexq/CS236/project-1/cmake-build-debug/CMakeFiles/project_1.dir/Project1_Starter_Code/Lexer.cpp.obj"
+  "C:/Users/alexq/CS236/project-1/Project1_Starter_Code/Token.cpp" "C:/Users/alexq/CS236/project-1/cmake-build-debug/CMakeFiles/project_1.dir/Project1_Starter_Code/Token.cpp.obj"
+  "C:/Users/alexq/CS236/project-1/Project1_Starter_Code/main.cpp" "C:/Users/alexq/CS236/project-1/cmake-build-debug/CMakeFiles/project_1.dir/Project1_Starter_Code/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Project1_Starter_Code"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/project_1.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_1.dir/Project1_Starter_Code/ColonAutomaton.cpp.obj"
+  "CMakeFiles/project_1.dir/Project1_Starter_Code/ColonDashAutomaton.cpp.obj"
+  "CMakeFiles/project_1.dir/Project1_Starter_Code/Lexer.cpp.obj"
+  "CMakeFiles/project_1.dir/Project1_Starter_Code/Token.cpp.obj"
+  "CMakeFiles/project_1.dir/Project1_Starter_Code/main.cpp.obj"
+  "libproject_1.dll.a"
+  "project_1.exe"
+  "project_1.exe.manifest"
+  "project_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
